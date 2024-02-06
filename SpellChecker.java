7d24d8af07c5ba1dad185fb0a77e82d5b5c2e375
TomerEinan <154810313+TomerEinan@users.noreply.github.com>
2024-02-06 16:45:50 +0200

    Update SpellChecker.java

--- SpellChecker.java
+++ SpellChecker.java
@@ -14,33 +14,36 @@ public class SpellChecker {
 		return str.substring(1);
 	}
 
-	public static int levenshtein(String word1, String word2) {
-		word1 = word1.toLowerCase();
-		word2 = word2.toLowerCase();
-		if (word1.length() == 0 ){
-			reutrn word2.length();
-		}
-		if (word2.length()== 0){
-			return word1.legnth();
-			}
-		if (word1.charAt(0) == word.charAt(0)){
-			return levenshtein(tail(word1), tail(word2));
-		}
-		return 1 + levenshtein(tail(word1), tail(word2));
-	}
+	public int levenshtein(String word1, String word2) {
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+
+    if (word1.length() == 0) {
+        return word2.length();
+    }
+    if (word2.length() == 0) {
+        return word1.length();
+    }
+    if (word1.charAt(0) == word2.charAt(0)) {
+        return levenshtein(tail(word1), tail(word2));
+    }
+    return 1 + levenshtein(tail(word1), tail(word2));
+}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		return "";
 	}
 
 }
